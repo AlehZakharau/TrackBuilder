db33906ccb432138a4ae2cd52063b73c99b873cb
Aleh Zaharau <38154177+AlehZakharau@users.noreply.github.com>
2023-05-10 04:09:34 -0400

    tuning point

---
+++ main/point_track2.go
@@ -0,0 +1,45 @@
+components {
+  id: "point"
+  component: "/main/point.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  properties {
+    id: "track"
+    value: "2.0"
+    type: PROPERTY_TYPE_NUMBER
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/assets/base.atlas\"\n"
+  "default_animation: \"circle\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  scale {
+    x: 0.1
+    y: 0.1
+    z: 0.1
+  }
+}
